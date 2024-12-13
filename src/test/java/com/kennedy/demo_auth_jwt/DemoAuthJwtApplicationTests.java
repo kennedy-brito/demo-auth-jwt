8ef2abf50f263aa58468c4221b3047f58e017eca
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-13 19:19:30 -0300

    Create Project

---
+++ src/test/java/com/kennedy/demo_auth_jwt/DemoAuthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kennedy.demo_auth_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoAuthJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

4baf16beec6d4625c9384dc372b24e85bc58c330
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-14 09:37:01 -0300

    configuring the authentication manager in SpringSecurityConfig

--- src/main/java/com/kennedy/demo_auth_jwt/config/SpringSecurityConfig.java
+++ src/main/java/com/kennedy/demo_auth_jwt/config/SpringSecurityConfig.java
@@ -3,6 +3,8 @@ package com.kennedy.demo_auth_jwt.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -42,4 +44,8 @@ public class SpringSecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
 }

ca6bd90b791a79e9d82603187b1c141405d72420
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-13 21:30:39 -0300

    create jwt util class and define constants, this class has operations that are useful to work with jwt tokens

---
+++ src/main/java/com/kennedy/demo_auth_jwt/jwt/JwtUtils.java
@@ -0,0 +1,12 @@
+package com.kennedy.demo_auth_jwt.jwt;
+
+public class JwtUtils {
+
+    public static final String JWT_BEARER = "Bearer ";
+    public static final String JWT_AUTHORIZATION = "Authorization";
+    public static final String SECRET_KEY = "0123456789-0123456789-0123456789";
+    public static final Long EXPIRE_DAYS = 0L;
+    public static final Long EXPIRE_HOURS = 0L;
+    public static final Long EXPIRE_MINUTES = 2L;
+
+}

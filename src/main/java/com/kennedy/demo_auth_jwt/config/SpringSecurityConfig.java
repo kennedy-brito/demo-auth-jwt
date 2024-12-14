21b8c2314397f59b57f3400040afa30ea145e19f
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-13 21:06:11 -0300

    Create SpringSecurity Configuration class

---
+++ src/main/java/com/kennedy/demo_auth_jwt/config/SpringSecurityConfig.java
@@ -0,0 +1,19 @@
+package com.kennedy.demo_auth_jwt.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
+
+@Configuration
+public class SpringSecurityConfig {
+
+
+}

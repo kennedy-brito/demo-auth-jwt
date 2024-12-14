0d90f5e43eabfdbaca078f82b3e27fd6c545aeac
kennedy_brito <willian.kennedy.brito@gmail.com>
2024-12-14 15:04:20 -0300

    Filtering the request using the jwt filter

--- src/main/java/com/kennedy/demo_auth_jwt/config/SpringSecurityConfig.java
+++ src/main/java/com/kennedy/demo_auth_jwt/config/SpringSecurityConfig.java
@@ -1,17 +1,17 @@
 package com.kennedy.demo_auth_jwt.config;
 
+import com.kennedy.demo_auth_jwt.jwt.JwtAuthorizationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
-import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import static org.springframework.security.web.util.matcher.AntPathRequestMatcher.antMatcher;
 
 @Configuration
@@ -36,7 +36,15 @@ public class SpringSecurityConfig {
                 .sessionManagement(
                         session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 )
+                .addFilterAfter(
+                    jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class
+                )
                 .build();
+    }
+
+    @Bean
+    public JwtAuthorizationFilter jwtAuthorizationFilter(){
+        return new JwtAuthorizationFilter();
     }
 
     @Bean
